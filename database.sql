d416939ec6d63fc56ff0feb9af58e272fb5c5cdc
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2025-12-03 12:04:19 +0100

    Creation of the customers table

--- database.sql
+++ database.sql
@@ -1,3 +1,11 @@
 create database bankly
 use bankly 
 
+create table  customers 
+(
+ customer_id int primary key  AUTO_INCREMENT  ,
+ full_name  varchar (250) ,
+ email varchar(250) ,
+ phone int ,
+ rege_date date 
+);
