f250a364bac55e4b09be05d16f204c745389b232
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2025-12-03 12:17:30 +0100

    insert a accounts  examples

--- requete.sql
+++ requete.sql
@@ -11,3 +11,10 @@ VALUES
 ('Ahmed', 'ahmed@bankly.com'),
 ('Sara', 'sara@bankly.com'),
 ('Nour', 'nour@bankly.com');
+
+INSERT INTO account (acc_num, balance, acc_type, customer_id, advisors_id)
+VALUES
+(1001, 5000, 'Checking', 1, 1),
+(1002, 12000, 'Savings', 2, 2),
+(1003, 3000, 'Business', 3, 3),
+(1004, 7500, 'Checking', 4, 4);
