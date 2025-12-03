852a26e83e75c032f933b58fe9f83f5e81880881
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2025-12-03 12:12:33 +0100

    insert a custumers examples

---
+++ requete.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers (full_name, email, phone, rege_date)
+VALUES
+('Ayoub', 'ayoub1@gmail.com', 0612345671, '2024-12-01'),
+('Ahmed', 'ahmed1@gmail.com', 0612345672, '2024-12-02'),
+('Sara', 'sara@gmail.com', 0612345673, '2024-12-03'),
+('Nour', 'nour@gmail.com', 0612345674, '2024-12-04');
