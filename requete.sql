cf90248f73af03aad5d9a3c7ea6ad6b03d6094d4
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2025-12-03 12:16:57 +0100

    insert a advisors  examples

--- requete.sql
+++ requete.sql
@@ -4,3 +4,10 @@ VALUES
 ('Ahmed', 'ahmed1@gmail.com', 0612345672, '2024-12-02'),
 ('Sara', 'sara@gmail.com', 0612345673, '2024-12-03'),
 ('Nour', 'nour@gmail.com', 0612345674, '2024-12-04');
+
+INSERT INTO advisors (full_name, email)
+VALUES
+('Ayoub', 'ayoub@bankly.com'),
+('Ahmed', 'ahmed@bankly.com'),
+('Sara', 'sara@bankly.com'),
+('Nour', 'nour@bankly.com');
